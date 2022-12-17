fe1df27d53448b44ed70e0c08b7b27e3ef635fb0
unknown <fajarcahyadiputra@gmail.com>
2022-12-17 13:57:15 +0700

    change

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -14,7 +14,7 @@ const {
 } = process.env;
 
 const startDb = async() => {
-    console.log("runsss");
+    console.log("okokokoko");
     
     if(!JWT_KEY){
         throw new Error("JWT_KEY must be defined in env")
