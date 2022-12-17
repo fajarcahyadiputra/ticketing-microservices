42237790eda6c12af1aa1cf604d03c853da560cd
unknown <fajarcahyadiputra@gmail.com>
2022-12-17 16:15:50 +0700

    some change

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 import { DatabaseConnectionError } from '@fajartickets/common/build/errors/database-connection-error';
 
 const startDb = async() => {
-    console.log("sdsfsf");
+    console.log("l0ading..l.");
     
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined in env")
